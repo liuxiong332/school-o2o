eceac622fcfd903b70f359a7e6393c290f0db32d
liuxiong332 <liuxiong332@163.com>
2020-03-03 18:37:18 +0800

    Make the primary key auto increment

--- src/main/java/xiong/o2o/entity/User.java
+++ src/main/java/xiong/o2o/entity/User.java
@@ -1,5 +1,7 @@
 package xiong.o2o.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 import java.util.Date;
@@ -20,6 +22,7 @@ public class User {
         this.updateTime = new Date();
     }
 
+    @TableId(type = IdType.AUTO)
     private Long id;
     private String name;
     private String password;
